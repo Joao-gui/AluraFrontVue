840fcfecdb9e8b45f6231705f646fa214bdb4983
Joao Guilherme Vianna Doveinis Pellacani <joao.pellacani@db1.com.br>
2019-09-02 12:40:49 -0300

    Commit Front Vue

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,74 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+    <!-- Comando para fazer um search -->
+    <!-- v-on:input é o comando que ele vai pegar o que foi escrito no Search -->
+    <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="filtre por parte do titulo">
+    <ul class="lista-fotos">
+      <!-- Aparece error mas funciona -->
+      <li class="lista-fotos-item" v-for="foto of fotos">
+        
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>         
+
+      </li>
+    </ul>
+
   </div>
 </template>
 
+<script>
+import Painel from './components/shared/painel/Painel.vue';
+export default {
+
+  components: {
+    'meu-painel' : Painel
+  },
+
+  data() {
+
+    return {
+
+      titulo: 'Alurapic', 
+      fotos: [],    
+      filtro: ''  
+    }
+  },
+  
+  created() {
+    //Pode omitir o let promise e o promise    
+    let promise = this.$http.get('http://localhost:3000/v1/fotos');
+    promise
+    .then(res => res.json())
+    .then(fotos => this.fotos = fotos, err => console.log(err));
+  }
+}
+
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.corpo{
+  font-family: Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
+.centralizado{
   text-align: center;
-  color: #2c3e50;
 }
-#nav {
-  padding: 30px;
+.lista-fotos{
+  list-style: none;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.lista-fotos .lista-fotos-item{
+  display: inline-block;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.imagem-responsiva{
+  width: 100%
+}
+.filtro{
+  display: block;
+  width: 100%;
 }
 </style>
+
